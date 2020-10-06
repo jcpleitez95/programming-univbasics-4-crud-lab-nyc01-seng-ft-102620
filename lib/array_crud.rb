947f983399626094141d847b4f17cf83d33d8fde
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-06 18:29:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["red", "orange", "yellow", "green"]
+  array = ["red", "orange", "yellow", "green"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+  array << 
 end
 
 def add_element_to_start_of_array(array, element)
